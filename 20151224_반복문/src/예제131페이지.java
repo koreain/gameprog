0326a247fe8b681808fafbc756ed7a6e2c7eded6
koreain <korea100th@gmail.com>
2015-12-24 17:38:27 +0900

    exercise added

---
+++ 20151224_반복문/src/예제131페이지.java
@@ -0,0 +1,16 @@
+
+public class 예제131페이지 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int ex=1;
+		for(int i=1; i<=10; i++){
+			ex*=i;
+		}
+		System.out.println(ex);*/
+	for(int i=1; i<=9; i++){
+		System.out.printf("%d*%d=%d\n",5,i,5*i);
+	}
+	}
+
+}

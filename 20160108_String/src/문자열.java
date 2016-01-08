9a8bda5497a93b92253793c5cdf959d67f66fa9d
koreain <korea100th@gmail.com>
2016-01-08 17:11:26 +0900

    20160108

---
+++ 20160108_String/src/문자열.java
@@ -0,0 +1,19 @@
+/*
+ * equals, length, trim, indexOf, lastIndexOf,
+ * valueOf, charAt
+ * toUpperCase : 대문자 변환
+ * toLowerCase : 소문자 변환
+ * 
+ * 
+ */
+public class 문자열 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String data="Hello String";
+		System.out.println(data.toUpperCase());
+		System.out.println(data.toLowerCase());
+		
+	}
+
+}

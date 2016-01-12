19e488a0f4e9dd0e3dd851a76069c8a63b8e9788
koreain <korea100th@gmail.com>
2016-01-12 17:52:08 +0900

    commit

--- 201601112_객체지향프로그램/src/멤버변수2.java
+++ 201601112_객체지향프로그램/src/멤버변수2.java
@@ -1,10 +1,19 @@
+import javax.swing.JOptionPane;
 
 public class 멤버변수2 {
 	static int hakbun;
 	static int kor,eng,math;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String str=JOptionPane.showInputDialog("국어점수:");
+		kor=Integer.parseInt(str);
+		str=JOptionPane.showInputDialog("영어점수:"); //str 덮어쓰기
+		eng=Integer.parseInt(str);
+		str=JOptionPane.showInputDialog("수학점수:"); //str 덮어쓰기
+		math=Integer.parseInt(str);
+		String result="총점:"+(kor+eng+math)+"\n"
+				     +"평균"+((kor+eng+math)/3);
+		JOptionPane.showInputDialog(null,result);
 	}
 
 }
